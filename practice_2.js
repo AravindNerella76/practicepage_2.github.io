1cd7639e4efa8f2db3749189f0c462141522fc34
AravindNerella76 <166791581+AravindNerella76@users.noreply.github.com>
2024-04-12 11:46:59 +0530

    Add files via upload

---
+++ practice_2.js
@@ -0,0 +1,208 @@
+// String operators
+
+// var a = "Suthra"
+// var b = 'Technologies'
+// var c = a+" "+b
+// console.log(c)
+
+// logical operators
+
+// var a = 10
+// var b = 20
+// console.log((a<b) || (a>b))
+
+// control statements
+
+//conditional control statements
+
+// var a = 400
+// var b = 200
+
+// if(a>b)
+// {
+//     console.log("if executed")
+// }
+// else if (a<b)
+// {
+//     console.log('if else executed')
+// }
+// else
+// {
+//     console.log("else executed")
+// }
+
+//looping / iterating statements
+
+// var a = 10
+// var b = 1
+
+// do
+// {
+//    console.log(b);
+//    b++;
+//    a--;
+// }while(a>8)
+
+//jumoing statements 
+
+//var a = "weeknd"
+
+// switch (a)
+// {
+//     case "weekend" :
+//         console.log("sunday");
+//     case "weekday" :
+//         console.log("weekday");
+//         continue;
+//     case "weeknd" :
+//         console.log("singer");
+        
+//     default :
+//         console.log("ok")        
+// }
+
+// for loop
+
+// for(var a = 1;a<=10;a++)
+// {
+//     console.log(a)
+// }
+
+// for of loop
+
+// var arr = [1,2,3,4,5,6,"Thala",8.7]
+
+// for(var a of arr)
+// {
+//     console.log(a)
+// }
+
+// for in loop
+
+// var o =
+// {
+//     namee : 'Aravind',
+//     age : 22
+// }
+// for(let a in o)
+// {
+//     console.log(o[a])
+// }
+
+//alert function
+
+// var a = "Aravind"
+// var b = "Nerella"
+
+// console.log(alert(a+" "+b))
+
+//confirm function
+
+// var a = "Aravind"
+// var b = "Nerella"
+
+// console.log(confirm(a+" "+b))
+
+//prompt function
+
+// var a = prompt("Enter a value")
+// var b = prompt('Enter a value')
+// var c = a+" "+b
+// console.log(c)
+
+// var a = parseFloat(prompt("Enter a value"))
+// var b = parseFloat(prompt("Enter b value"))
+// console.log(a/b)
+
+// var a = parseInt(prompt("Enter a value"))
+// var c = Math.sqrt(a)
+// console.log(c)
+
+//user Defined Functions
+
+// function declarations
+
+// function display(a,b)
+// {    
+//     alert("Display Function")
+// }
+// display(4,5)
+
+//function expression
+
+// var a = 8
+// var b = 9
+// var display = function()
+// {    
+//     alert("Hey hi")
+// }
+
+// var fun = function(a,b)
+// {
+//     alert("Hey hi")
+// }
+// fun(3,4)
+
+// var display = (a,b)=>
+// {
+//     c = a+b
+//     return c;
+// }
+// var a = parseInt(prompt("enter a"))
+// var b = parseInt(prompt("enter b" ))
+// alert((display(a,b)))
+// console.log(display(a,b)) 
+
+// function display(inp)
+// {
+//     alert("The Name is changed to"+" "+inp)
+// }
+
+//ways of creating an array
+
+
+
+// var ar2 = []
+// ar2[0] = 1
+// ar2[1] = 2
+// ar2[2] = 3
+// ar2[3] = 'Aravind'
+// console.log(ar2)
+
+// var ar3 = new Array(1,2,3,"weeknd",4,5)
+// console.log(ar3)
+
+
+// var ar1 = [1,2,3,45,6]
+// console.log(ar1)
+// ar1.splice(2)
+// console.log(ar1)
+
+// var  obj = 
+// {
+//     firstName : "Rakesh",
+//     secondName : "sarada",
+//     age : 23
+// }
+// obj.age = 69
+// console.log(obj)
+
+
+// var abj = {}
+
+// abj.firstName = "srinu"
+// abj.secondName= "chimpu"
+
+// console.log(abj.firstName)
+
+
+// var obj1 = new Object()
+
+// obj1.username = "moosa"
+// obj1.password = "gambhir@123"
+
+// console.log(obj1.password)
+
+// doms
+
+// document.getElementById("routed").innerHTML ="super ra thammudu kummesav"
